8e355f48984527f80366716cb3b8316f86dd27c8
Sorin Otescu <sorinu@gmail.com>
2018-08-10 19:45:39 +0300

    Improve Kafka producer

--- scripts/kafka_producer.py
+++ scripts/kafka_producer.py
@@ -18,29 +18,20 @@ class MyConsumerRebalanceListener(ConsumerRebalanceListener):
         print("on_partitions_revoked: %s" % partitions)
 
 
-def produce(servers, certs_path_prefix, topic, partition, data):
-    if partition is None:
-        print("Connecting to Kafka servers %s and sending to topic '%s' on random partition" %
-              (servers, topic))
-    else:
-        print("Connecting to Kafka servers %s and sending to topic '%s' on partition %d" %
-              (servers, topic, partition))
-
-    consumer = new_kafka_json_producer(bootstrap_servers=servers,
-                                       ssl_path_prefix=certs_path_prefix)
-
-    consumer.send(topic, value=data, partition=partition).get(10000)
-    consumer.close()
-
-
-if __name__ == '__main__':
+def _parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-b', '--bootstrap-servers', help='Kafka bootstrap servers (comma separated)', required=True)
     parser.add_argument('-g', '--group', help='Kafka consumer group name', default='iris-daemons')
     parser.add_argument('-C', '--certs-path-prefix', help='Kafka certificate files path prefix',
                         default=None)
     parser.add_argument('topic_partition', help='Kafka topic/partition to send to, like "topic[:partition]"')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def _main():
+    args = _parse_args()
+
+    servers = args.bootstrap_servers.split(',')
 
     try:
         topic, partition = args.topic_partition.split(':')
@@ -49,9 +40,22 @@ if __name__ == '__main__':
         topic = args.topic_partition
         partition = None
 
-    data = json.load(sys.stdin)
+    if partition is None:
+        print("Connecting to Kafka servers %s and sending to topic '%s' on random partition" %
+              (servers, topic))
+    else:
+        print("Connecting to Kafka servers %s and sending to topic '%s' on partition %d" %
+              (servers, topic, partition))
 
-    produce(servers=args.bootstrap_servers.split(','),
-            certs_path_prefix=args.certs_path_prefix,
-            topic=topic, partition=partition,
-            data=data)
+    consumer = new_kafka_json_producer(bootstrap_servers=servers,
+                                       ssl_path_prefix=args.certs_path_prefix)
+
+    for line in sys.stdin.readlines():
+        data = json.loads(line)
+        consumer.send(topic, value=data, partition=partition).get(10000)
+
+    consumer.close()
+
+
+if __name__ == '__main__':
+    _main()
