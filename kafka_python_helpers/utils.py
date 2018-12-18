8f7ae8cf58b56c9e73b0462dfe5e2602f69119cc
Sorin Otescu <sorinu@gmail.com>
2018-12-18 22:20:26 +0200

    Fix behavior of 'compact_int_list' for single non-consecutive values

--- kafka_python_helpers/utils.py
+++ kafka_python_helpers/utils.py
@@ -7,15 +7,22 @@ def compact_int_list(lst):
     if len(lst) == 0:
         return []
     elif len(lst) == 1:
-        return [(lst[0], lst[0])]
+        return lst
 
     results = []
 
     start = lst[0]
     for a, b in zip(lst, islice(lst, 1, None)):
         if b != a + 1:
-            results.append((start, a))
+            if a != start:
+                results.append((start, a))
+            else:
+                results.append(start)
             start = b
 
-    results.append((start, lst[-1]))
+    if start != lst[-1]:
+        results.append((start, lst[-1]))
+    else:
+        results.append(start)
+
     return results
